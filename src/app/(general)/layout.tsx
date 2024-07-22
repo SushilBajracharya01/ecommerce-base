0f957e80bc125948e3bcb477e81898570e65312f
Sushil <bajracharya.sushil01@gmail.com>
2024-07-22 23:36:17 +0545

    feat: add react-hot-toast and validation libraries

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import SessionProvider from "@/app/SessionProvider"
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`min-w-96 ${inter.className}`}>
         <SessionProvider>
-            {children}
+          <Toaster position="top-center" />
+
+          {children}
         </SessionProvider>
       </body>
     </html >
